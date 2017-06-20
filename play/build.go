6fc96948bc58978926161434197a28a747118966
Phong Nguyen <pnguyen@hydrasoft.io>
2017-06-20 19:08:18 -0400

    Refactor play docker images

--- play/build.go
+++ play/build.go
@@ -8,9 +8,7 @@ func main() {
 	executor := executor.Create("docker-play")
 
 	executor = executor.Add("dev tag")
-	executor = executor.Add("./build-play-base `sem-info tag latest`")
 	executor = executor.Add("./build-play `sem-info tag latest` `sem-info tag latest`")
-	executor = executor.Add("./build-play-crypto `sem-info tag latest` `sem-info tag latest`")
 	executor = executor.Add("./build-play-builder `sem-info tag latest` `sem-info tag latest`")
 	executor = executor.Add("./build-update-readme `sem-info tag latest`")
 
