cbb6670686a231ede8b483fc8f2fdbe07ee74f7c
Chris Hicks <chicks@flow.io>
2017-06-26 14:22:05 -0400

    add node base image / tools

---
+++ node/build.go
@@ -0,0 +1,14 @@
+package main
+
+import (
+	"github.com/flowcommerce/tools/executor"
+)
+
+func main() {
+	executor := executor.Create("docker-node")
+
+	executor = executor.Add("dev tag")
+	executor = executor.Add("./build-node `sem-info tag latest` `sem-info tag latest`")
+
+	executor.Run()
+}
