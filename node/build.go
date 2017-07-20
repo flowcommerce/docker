9b34fce4d69a1ea1c0bf21d99f7b0fbe6b18a7bc
Phong Nguyen <pnguyen@hydrasoft.io>
2017-07-20 15:55:31 -0400

    Add npm-cli-login and awscli to node image

--- node/build.go
+++ node/build.go
@@ -8,8 +8,7 @@ func main() {
 	executor := executor.Create("docker-node")
 
 	executor = executor.Add("dev tag")
-	executor = executor.Add("./build-node `sem-info tag latest` `sem-info tag latest`")
-	executor = executor.Add("./build-node-builder `sem-info tag latest` `sem-info tag latest`")
+	executor = executor.Add("./build-node `sem-info tag latest`")
 
 	executor.Run()
 }
