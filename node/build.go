82d07db740aae4900b5e7d5af55c5ceb1f2b686f
Phong Nguyen <pnguyen@users.noreply.github.com>
2017-07-20 15:59:34 -0400

    Add npm-cli-login and awscli to node image (#20)
    * Add bzip2 and create log dir for node image

--- node/build.go
+++ node/build.go
@@ -8,7 +8,7 @@ func main() {
 	executor := executor.Create("docker-node")
 
 	executor = executor.Add("dev tag")
-	executor = executor.Add("./build-node `sem-info tag latest` `sem-info tag latest`")
+	executor = executor.Add("./build-node `sem-info tag latest`")
 
 	executor.Run()
 }
