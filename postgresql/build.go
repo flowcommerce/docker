14193521dbe895f0b7871b824278038b107ec3f7
Matthew Mark Miller <m3@dasmb.com>
2019-01-18 16:55:48 -0500

    Updates to all base dockerfiles

--- postgresql/build.go
+++ postgresql/build.go
@@ -11,8 +11,9 @@ import (
 
 func main() {
 	executor := executor.Create("docker-postgresql")
-	image := fmt.Sprintf("flowdocker/postgresql:%s", latestTag())
+	image := fmt.Sprintf("479720515435.dkr.ecr.us-east-1.amazonaws.com/postgresql:%s", latestTag())
 
+	executor = executor.Add("eval $(aws ecr get-login --no-include-email)")
 	executor = executor.Add(fmt.Sprintf("docker build -t %s .", image))
 	executor = executor.Add(fmt.Sprintf("docker push %s", image))
 
@@ -21,6 +22,6 @@ func main() {
 
 func latestTag() string {
 	tag, err := exec.Command("sem-info", "tag", "latest").Output()
-	util.ExitIfError(err, fmt.Sprintf("Error running sem-info tag latest"))	
+	util.ExitIfError(err, fmt.Sprintf("Error running sem-info tag latest"))
 	return strings.TrimSpace(string(tag))
 }
