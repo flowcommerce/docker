4cd823c8bb524e100d78809f5e16e258681fa4ca
Ben Iofel <ben@flow.io>
2020-01-02 15:10:18 -0500

    Build java 13 images

--- play/build.go
+++ play/build.go
@@ -8,9 +8,10 @@ func main() {
 	executor := executor.Create("docker-play")
 
 	executor = executor.Add("dev tag")
-	executor = executor.Add("./build-play `sem-info tag latest` `sem-info tag latest`")
-	executor = executor.Add("./build-play-builder `sem-info tag latest` `sem-info tag latest`")
-	executor = executor.Add("./build-update-readme `sem-info tag latest`")
+	executor = executor.Add("./build-play `sem-info tag latest` 8")
+	executor = executor.Add("./build-play `sem-info tag latest` 13")
+	executor = executor.Add("./build-play-builder `sem-info tag latest` 8")
+	executor = executor.Add("./build-play-builder `sem-info tag latest` 13")
 
 	executor.Run()
 }
