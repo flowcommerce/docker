73346777fd7d7942098215b9da5b5af3fee959a1
Gregor Heine <gregor@flow.io>
2022-10-28 14:09:11 +0100

    Fetch environment-provider version from s3

--- node/helpers.rb
+++ node/helpers.rb
@@ -8,20 +8,3 @@ def run(command, quiet=false)
   end
   result
 end
-
-def get_env_providor_location()
-  possible_paths = [
-    '/../../environment-provider',
-    '/../../../flowvault/environment-provider'
-  ];
-  expanded_paths = possible_paths.map { |path| File.expand_path(File.join(File.dirname(__FILE__), path)) }
-  found_path = expanded_paths.find { |path| File.directory?(path) }
-
-  if found_path == nil
-    puts "ERROR finding environment-provider repo"
-    exit(1)
-  end
-
-  puts "Found environment-provider repo at: [#{found_path}]"
-  found_path
-end
