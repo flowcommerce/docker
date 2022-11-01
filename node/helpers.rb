5de91178c6b58b76adae3ce75e4fec500ecd56d2
himanshuupadhyay101 <70565558+himanshuupadhyay101@users.noreply.github.com>
2022-11-01 17:25:52 +0530

    revert rescue

--- node/helpers.rb
+++ node/helpers.rb
@@ -2,11 +2,6 @@ def run(command, quiet=false, fail=true)
   if quiet == false
     puts "  %s" % command
   end
-  begin
-    if fail == false
-  rescue
-    puts "exception rescued"
-  end
   result = system(command)
   if result != true
     raise "COMMAND FAILED:  %s" % command
