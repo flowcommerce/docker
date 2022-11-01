06c7ae0115627cd52acbd807e87ce50fc21384de
himanshuupadhyay101 <70565558+himanshuupadhyay101@users.noreply.github.com>
2022-11-01 18:08:16 +0530

    add exception handling

--- node/helpers.rb
+++ node/helpers.rb
@@ -1,10 +1,14 @@
 def run(command, quiet=false, fail=true)
+ begin
   if quiet == false
     puts "  %s" % command
   end
   if fail == false
     puts ""
   end
+ rescue
+   puts "exception rescued"
+ end
   result = system(command)
   if result != true
     raise "COMMAND FAILED:  %s" % command
